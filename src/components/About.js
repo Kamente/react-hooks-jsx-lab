25a16bb289dcca5f49673410a1889ba29dec5a4f
Kamente <justinm48000@gmail.com>
2023-07-19 22:28:37 +0300

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
+import Styles from "../styles.css";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am a passionate web developer with a love for creating innovative websites and applications.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
